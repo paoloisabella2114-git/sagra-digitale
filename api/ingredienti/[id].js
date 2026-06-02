bafc42d3e8f26734d0ee829f708a075035628e16
paoloisabella2114-git <paolo.isabella2114@gmail.com>
2026-06-02 21:52:34 +0200

    Create [id].js

---
+++ api/ingredienti/[id].js
@@ -0,0 +1,19 @@
+const db = require('../../lib/db');
+
+module.exports = async (req, res) => {
+  try {
+    const { id } = req.query;
+    const result = await db.query(`
+      SELECT
+        ai.id,
+        i.descrizione,
+        i.descrizionebreve,
+        i.prezzo,
+        ai.quantita_massima,
+        ai.obbligatorio,
+        ai.posizione
+      FROM articoli_ingredienti ai
+      JOIN ingredienti i ON i.id = ai.id_ingrediente
+      WHERE ai.id_articolo = $1
+        AND ai.visibile IS NOT FALSE
+      ORDE
